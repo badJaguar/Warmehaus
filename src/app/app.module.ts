d046fda0c9197d2e4dc032c0a99f5868ff884ca1
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-29 12:47:58 +0300

    header menu added and footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './menu/footer/footer.component';
+import { DefaultPageComponent } from './default-page/default-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent,
+    DefaultPageComponent
   ],
   imports: [
     BrowserModule,
