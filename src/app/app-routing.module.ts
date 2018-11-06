be0c41b7a7ee411855cdc5b6187c39967ec42ed1
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2018-11-07 00:40:16 +0300

    апдейт

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultPageComponent } from './default-page/default-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DefaultPageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
