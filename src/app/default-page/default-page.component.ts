f440840f208dcf01a14c5f77e5ea29661d08ca4f
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-11-29 14:22:41 +0300

    Update pics

--- src/app/default-page/default-page.component.ts
+++ src/app/default-page/default-page.component.ts
@@ -14,15 +14,15 @@ export interface Tile {
 })
 export class DefaultPageComponent implements OnInit {
   tiles: Tile[] = [
-    {url: "../../assets/images/gridPics/Logo_Banner.png", cols: 4, rows: 1, color: 'lightblue'},
-    {url: "../../assets/images/gridPics/Нагревательный_Кабель.png", cols: 2, rows: 1, color:'#cacaca'},
-    {url: "../../assets/images/gridPics/Инфракрасная_Плёнка.png", cols: 2, rows: 1, color: '#DDBDF1'},
-    {url: "../../assets/images/gridPics/Нагревательный_Мат.png", cols: 2, rows: 1, color: '#DDBDF1'},
+    {url: "../../assets/images/gridPics/Слайд.png", cols: 4, rows: 1, color: 'lightblue'},
+    {url: "../../assets/images/gridPics/Нагревательный_Мат_160w.png", cols: 2, rows: 1, color:'#cacaca'},
+    {url: "../../assets/images/gridPics/Нагревательный_Мат_160w.png", cols: 2, rows: 1, color: '#DDBDF1'},
+    {url: "../../assets/images/gridPics/Нагревательный_Мат_160w.png", cols: 2, rows: 1, color: '#DDBDF1'},
 
-    {url: 'Five', cols: 2, rows: 1, color: 'lightpink'},
-    {url: 'Six', cols: 2, rows: 1, color: '#DDBDF1'},
-    {url: 'Seven', cols: 1, rows: 1, color: 'lightpink'},
-    {url: 'Eight', cols: 1, rows: 1, color: '#DDBDF1'},
+    {url: "../../assets/images/gridPics/Нагревательный_Мат_160w.png", cols: 2, rows: 1, color: 'lightpink'},
+    {url: "../../assets/images/gridPics/Нагревательный_Мат_160w.png", cols: 2, rows: 1, color: '#DDBDF1'},
+    {url: "../../assets/images/gridPics/Нагревательный_Мат_160w.png", cols: 1, rows: 1, color: 'lightpink'},
+    {url: "../../assets/images/gridPics/Нагревательный_Мат_160w.png", cols: 1, rows: 1, color: '#DDBDF1'},
     {url: 'Eight', cols: 4, rows: 1, color: '#DDBDF1'},
   ];
   constructor() { }
