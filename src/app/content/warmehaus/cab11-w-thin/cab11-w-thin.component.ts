859006a63bce8669ded4417a93f30a08e385cf7a
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-12-10 16:08:18 +0300

    IDs changed

--- src/app/content/warmehaus/cab11-w-thin/cab11-w-thin.component.ts
+++ src/app/content/warmehaus/cab11-w-thin/cab11-w-thin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { ELEMENT_DATA_MAT_CAB_11W_THIN } from 'src/models/cab-11w-thin';
 
 @Component({
   selector: 'app-cab11-w-thin',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Cab11WThinComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  displayedColumns: string[] = ['name', 'nominal', 'price'];
+    dataSource1 = new MatTableDataSource(ELEMENT_DATA_MAT_CAB_11W_THIN);
+  
+    @ViewChild(MatSort) sort: MatSort;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+  
+    ngOnInit() {
+      this.dataSource1.sort = this.sort;
+      this.dataSource1.paginator = this.paginator;
+    }
+    applyFilter(filterValue: string) {
+      this.dataSource1.filter = filterValue.trim().toLowerCase();
+    }
 }
