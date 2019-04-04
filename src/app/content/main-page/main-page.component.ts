585fe75f1f7dea74de81c961c1636ed5fff781d9
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-04-04 23:58:08 +0300

    Pathes simplifying

--- src/app/content/main-page/main-page.component.ts
+++ src/app/content/main-page/main-page.component.ts
@@ -11,7 +11,7 @@ export class MainPageComponent implements OnInit {
   tiles: ITile[] = [
     {
       route: '#',
-      picUrl: '../../assets/images/gridPics/grid-1-web.jpg',
+      picUrl: 'assets/images/gridPics/grid-1-web.jpg',
       cols: 4,
       rows: 2,
       alt: 'Теплые полы в Минске'
@@ -19,21 +19,21 @@ export class MainPageComponent implements OnInit {
     {
       route:
         '/warmehaus/mat-160W', picUrl:
-        '../../assets/images/gridPics/Нагревательный_Мат_160Вт.png',
+        'assets/images/gridPics/Нагревательный_Мат_160Вт.png',
       cols: 2,
       rows: 1,
       alt: 'Нагревательный мат 160 Вт'
     },
     {
       route: '/warmehaus/mat-200W',
-      picUrl: '../../assets/images/gridPics/Нагревательный_Мат_200Вт.png',
+      picUrl: 'assets/images/gridPics/Нагревательный_Мат_200Вт.png',
       cols: 2,
       rows: 1,
       alt: 'Нагревательный мат 200 Вт'
     },
     {
       route: '/warmehaus/cab-11W-thin',
-      picUrl: '../../assets/images/gridPics/Нагревательный_Кабель_в_слой_11Вт.png',
+      picUrl: 'assets/images/gridPics/Нагревательный_Кабель_в_слой_11Вт.png',
       cols: 2,
       rows: 1,
       alt: 'Нагревательный Кабель в слой плиточного клея 11Вт'
@@ -41,35 +41,35 @@ export class MainPageComponent implements OnInit {
 
     {
       route: '/warmehaus/cab-14W-thin',
-      picUrl: '../../assets/images/gridPics/Нагревательный_Кабель_в_слой_14Вт.png',
+      picUrl: 'assets/images/gridPics/Нагревательный_Кабель_в_слой_14Вт.png',
       cols: 2,
       rows: 1,
       alt: 'Нагревательный Кабель в слой плиточного клея 14Вт'
     },
     {
       route: '/warmehaus/cab-20W-uv-protection',
-      picUrl: '../../assets/images/gridPics/Нагревательный_Кабель_в_стяжку_20Вт.png',
+      picUrl: 'assets/images/gridPics/Нагревательный_Кабель_в_стяжку_20Вт.png',
       cols: 2,
       rows: 1,
       alt: 'Нагревательный Кабель в стяжку 20Вт'
     },
     {
       route: '/warmehaus/films',
-      picUrl: '../../assets/images/gridPics/Инфракрасная_Плёнка.png',
+      picUrl: 'assets/images/gridPics/Инфракрасная_Плёнка.png',
       cols: 2,
       rows: 1,
       alt: 'Инфракрасная пленка'
     },
     {
       route: 'url',
-      picUrl: '../../assets/images/gridPics/Системы_антиобледенения.png',
+      picUrl: 'assets/images/gridPics/Системы_антиобледенения.png',
       cols: 2,
       rows: 1,
       alt: 'Системы антиобледенения'
     },
     {
       route: '/warmehaus/termostats',
-      picUrl: '../../assets/images/gridPics/Терморегуляторы.png',
+      picUrl: 'assets/images/gridPics/Терморегуляторы.png',
       cols: 2,
       rows: 1,
       alt: 'Терморегуляторы'
